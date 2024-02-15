4d415b5bc43eee9398b15b72bb2dc877716ab82d
EricSousa02 <ericsousadeandrade@gmail.com>
2024-02-15 09:30:40 -0300

    loader

--- components/Loader.tsx
+++ components/Loader.tsx
@@ -2,13 +2,15 @@ import Image from "next/image";
 
 const Loader = () => (
   <div className='flex h-screen w-screen flex-col items-center justify-center gap-2'>
-    <Image
-      src='/assets/loader.gif'
-      alt='loader'
-      width={100}
-      height={100}
-      className='object-contain'
-    />
+    <div className='loader'>
+      <div className='loader-square'></div>
+      <div className='loader-square'></div>
+      <div className='loader-square'></div>
+      <div className='loader-square'></div>
+      <div className='loader-square'></div>
+      <div className='loader-square'></div>
+      <div className='loader-square'></div>
+    </div>{" "}
     <p className='text-sm font-bold text-primary-grey-300'>Carregando...</p>
   </div>
 );
